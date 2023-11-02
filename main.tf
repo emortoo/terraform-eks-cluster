7f56166186c8dfa7df4cae80c29421d2a6218fb0
Emmanuel Mortoo <emortoo@yahoo.com>
2023-11-02 17:28:41 -0400

    added gitactions

--- main.tf
+++ main.tf
@@ -10,6 +10,8 @@ provider "kubernetes" {
 
 provider "aws" {
   region = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 data "aws_availability_zones" "available" {}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,13 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+variable "access_key" {
+  description = "access_key"
+  type        = string
+  default     = "AKIA4GBGJZTOSJGKORNB"
+}
+variable "secret_key" {
+  description = "secret_key"
+  type        = string
+  default     = "VPU67t2D7Wr6EYh90sp+zfa94ue6Iffn0aIHb1SG"
+}
